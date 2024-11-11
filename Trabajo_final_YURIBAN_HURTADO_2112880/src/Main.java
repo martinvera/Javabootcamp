9a0d7eea5c397efe61b7237fb433c80b980d1938
Yuriban <yurix12k@gmail.com>
2024-11-10 19:07:14 -0500

    Trabajo final Yuriban Hurtado 2112880

---
+++ Trabajo_final_YURIBAN_HURTADO_2112880/src/Main.java
@@ -0,0 +1,15 @@
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        //Yuriban Miguel Hurtado Camacho - 2112880
+        Cajero c = new Cajero();
+        c.procesoPrincipal();
+
+    }
+
+}
