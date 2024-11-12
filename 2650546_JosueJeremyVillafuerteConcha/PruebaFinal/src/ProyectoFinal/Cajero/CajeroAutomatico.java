9fd55e444bb2d9dbce94574c7eff73e11f3904cf
Josué Jeremy Villafuerte Concha <103544104+JosueJVC@users.noreply.github.com>
2024-11-11 21:21:18 -0500

    Correcciones del Proyecto Final

--- 2650546_JosueJeremyVillafuerteConcha/PruebaFinal/src/ProyectoFinal/Cajero/CajeroAutomatico.java
+++ 2650546_JosueJeremyVillafuerteConcha/PruebaFinal/src/ProyectoFinal/Cajero/CajeroAutomatico.java
@@ -2,11 +2,14 @@ package ProyectoFinal.Cajero;
 
 import ProyectoFinal.Banco.Constantes;
 import ProyectoFinal.Banco.Convertidor;
+
+import java.sql.SQLOutput;
 import java.text.NumberFormat;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.Locale;
 import java.util.Scanner;
+import java.util.TimeZone;
 
 public class CajeroAutomatico {
 
@@ -57,32 +60,44 @@ public class CajeroAutomatico {
     }
 
     protected String validarTarjeta(){
-        while (true) {
+        int cont = 1;
+        while (cont <= Constantes.INTENTOS) {
             System.out.print("Ingrese el número de tarjeta (16 dígitos): ");
             numeroTarjeta = scanner.nextLine();
-            if (numeroTarjeta.matches("\\d{16}")) {
+            if (getNumeroTarjeta().matches("\\d{16}")) {
+                setNumeroTarjeta(numeroTarjeta);
                 return numeroTarjeta;
             } else {
-                System.out.println("Número de tarjeta inválido. Debe contener 16 dígitos.");
+                System.out.println(Constantes.MENSAJE_TARJETA);
             }
+            cont++;
         }
+        System.out.println(Constantes.MENSAJE_FINAL);
+        System.exit(1);
+        return "";
     }
 
     protected int monedaRetiro(){
-        while (true) {
+        int cont = 1;
+        while (cont <= Constantes.INTENTOS) {
             System.out.print("Opción (1 o 2): ");
             int opcionMoneda = scanner.nextInt();
             if (opcionMoneda == 1 || opcionMoneda == 2) {
                 return opcionMoneda;
             } else {
-                System.out.println("Opción inválida. Intente nuevamente.");
+                System.out.println(Constantes.MENSAJE_MONEDA);
             }
+            cont++;
         }
+        System.out.println(Constantes.MENSAJE_FINAL);
+        System.exit(1);
+        return 0;
     }
 
     protected double retirarSaldo(int opcionMoneda){
+        int cont = 1;
         double montoConvertido = 0;
-        while (true) {
+        while (cont <= Constantes.INTENTOS) {
             System.out.print("Ingrese el monto a retirar: ");
             double montoSolicitado = scanner.nextInt();
             if (montoSolicitado > 0) {
@@ -92,19 +107,23 @@ public class CajeroAutomatico {
                         saldo -= montoConvertido;
                         return montoSolicitado;
                     } else {
-                        System.out.println("Saldo insuficiente en soles.");
+                        System.out.println(Constantes.MENSAJE_RETIRO_1);
                     }
                 } else if (opcionMoneda == 1 && montoSolicitado <= saldo) {
                     saldo -= montoSolicitado;
                     montoConvertido = montoSolicitado;
                     return montoSolicitado;
                 } else {
-                    System.out.println("Saldo insuficiente en soles.");
+                    System.out.println(Constantes.MENSAJE_RETIRO_1);
                 }
             } else {
-                System.out.println("El monto debe ser mayor a cero.");
+                System.out.println(Constantes.MENSAJE_RETIRO_2);
             }
+            cont++;
         }
+        System.out.println(Constantes.MENSAJE_FINAL);
+        System.exit(1);
+        return 0;
     }
 
     protected void imprimirBoucher(int opcionMoneda, double montoSolicitado){
@@ -114,8 +133,13 @@ public class CajeroAutomatico {
         System.out.println("Nombre Completos: " + getNombres() + " " + getApellidos());
         System.out.println("Monto retirado: " + montoSolicitado + " " + moneda + " (" + Convertidor.numeroALetras((int) montoSolicitado) + " con ceros " + moneda + ")");
         System.out.println("Saldo restante: " + NumberFormat.getCurrencyInstance(new Locale("es", "PE")).format(getSaldo()));
-        // Fecha y hora de la transacción
+        // Fecha y hora de la transacción - Zona
         SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy HH:mm:ss");
+        sdf.setTimeZone(TimeZone.getTimeZone(Constantes.ZONA_HORARIA));
         System.out.println("Fecha y hora de la transacción: " + sdf.format(new Date()));
+    }
+
+    protected void mensajeInicial(){
+        System.out.println("--- Bienvenido al Cajero Automatico --- \nPor favor ingresar los datos solicitados. Por su seguridad, solamente tienes " + Constantes.INTENTOS + " intentos disponibles");
     }
 }
